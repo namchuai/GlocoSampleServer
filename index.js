7a27a38f2c83c94d37eeb0dad18391bec878022c
Nam Nguyen <nam.nh1@altplus.com.vn>
2017-04-21 11:25:09 +0700

    add doctor api

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var http = require('http')
+var fs = require('fs')
 var app = express()
 
 app.get('/', function (req, res) {
   res.send('Welcome, Nam!')
 })
+
+app.get('/doctors', function (req, res) {
+   fs.readFile("doctors.json", 'utf8', function (err, data) {
+       res.end(data);
+   });
+})
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('listening on 3000');
